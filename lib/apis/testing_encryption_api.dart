bbc495678efb1e85167b05ff090e28470c9fb934
slmnkhn79 <slmnkhn79@gmail.com>
2024-12-30 15:31:33 +0530

    working on encryption

--- lib/apis/testing_encryption_api.dart
+++ lib/apis/testing_encryption_api.dart
@@ -1,49 +1,33 @@
 import 'dart:convert';
+import 'package:encrypt/encrypt_io.dart';
+import 'package:pointycastle/asymmetric/api.dart';
 import 'package:quizzie_thunder/utils/dio_client.dart';
 import 'package:rsa_encrypt/rsa_encrypt.dart';
-import 'package:pointycastle/asymmetric/api.dart';
+import 'package:pointycastle/export.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
-final publicKeyPem = '''
------BEGIN PUBLIC KEY-----
+final publicKeyPem = '''-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCmCFiFZyqgg13mrl2t9eOLaIGB
 oFBBV6PvcIVv39/tmhdrm9hnWY7aZgcg1r7ppG+Wx49UKVPCB2Kjm143zras6oRd
 rzQrK+BrQeFbNIfnxIQKuXupaZQqdgZFvO5Su2eiexGLgr/a6m/8LO5bKyS5ZGPd
 N6BYLhZfasEb9qKg6wIDAQAB
------END PUBLIC KEY-----
-''';
+-----END PUBLIC KEY-----''';
 
 class TestingEncryptionApi {
-Future<void> encryptAndSendData(String message) async{
-    final publicKey = RsaKeyHelper().parsePublicKeyFromPem(publicKeyPem);
-
-    // Convert message to bytes
-
-    // Encrypt the message bytes
-    final encryptedMessageBytes = encrypt(message, publicKey);
-    final messageBytes = utf8.encode(encryptedMessageBytes);
-
-    final encryptedMessageBase64 = base64Encode(messageBytes);
-    print('encryptedMessageBase64: $encryptedMessageBase64');
-    await sendEncryptData(encryptedMessageBase64);
-    // return encryptedMessageBase64;
-
-    // Send `encryptedMessageBase64` to Parse Server
+Future<void> encryptAndSendData(String message) async {
+  final publicKey = await parseKeyFromFile<RSAPublicKey>(publicKeyPem);
+    final encrypter = encrypt.Encrypter(encrypt.RSA(publicKey: rsaParser));
+    final encryptedPassword = encrypter.encrypt("Message").base64;
+    sendEncryptData(encryptedPassword);
   }
-
-  // List<int> encrypt(List<int> data, RSAPublicKey publicKey) {
-  //   final cipherText = RSAP (data, publicKey);
-  //   print('ciphertext:- $cipherText');
-  //   return cipherText;
-  // }
-
-  Future<void> sendEncryptData(String encodedDate) async {
+  Future<void> sendEncryptData(String encodedData) async {
     try {
       final response = await DioClient.getDioInstance().post(
-        "/decryptData", data:{'encryptedMessage': encodedDate}
+        "/decryptData", data: {'encryptedMessage': encodedData}
       );
-      print("decryptedMesage: ${response.data['result']}");
+      print("decryptedMessage: ${response.data['result']}");
     } catch (e) {
-      print('encrytion failed ${e.toString()}');
+      print('encryption failed ${e.toString()}');
     }
   }
 }
